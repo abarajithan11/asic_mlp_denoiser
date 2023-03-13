2d05c91fde825c99b2ac40577dcd1e1deb67c5dc
Aba <abarajithan-7@gmail.com>
2023-03-13 13:52:40 -0700

    zero pad inputs

--- src/mlp_98.sv
+++ src/mlp_98.sv
@@ -27,31 +27,52 @@ module mlp_98 #(
   logic [W_K-1:0] weights_n2     [N2-1:0];
   weights_luts luts (.*);
 
-  // Adder Trees
-  logic [D1  :0][N1/2-1:0][N2-1:0][W_A_POL_SUM-1:0] a_pol_tree_i, a_pol_tree_o;
-  logic [D1  :0][N1/2-1:0][N2-1:0][W_A_MAG_SUM-1:0] a_mag_tree_i, a_mag_tree_o;
-  logic [N2-1:0][W_A_SUM-1:0] sum_a;
-  logic [D2  :0][N2-1:0][W_Y_SUM-1:0] b_tree;
-
+  // Zero padding
   genvar d, n1, n2, a;
+  localparam  N12_PAD = 2**D1,
+              N2_PAD  = 2**D2;
+
+  logic [N12_PAD-1:0][W_X-1:0] in_mag_pad;
+  logic [N12_PAD-1:0]          in_pol_pad;
+  logic [W_K-1:0] weights_n1_mag_pad [N12_PAD-1:0][N2_PAD-1:0]; 
+  logic [W_K-1:0] weights_n1_pol_pad [N12_PAD-1:0][N2_PAD-1:0];
+  logic [W_K-1:0] weights_n2_pad     [N2_PAD -1:0];
+
+  assign in_mag_pad = {'0, in_mag};
+  assign in_pol_pad = {'0, in_pol};
+
+  for (n2=0; n2<N2_PAD; n2=n2+1) begin
+    assign weights_n2_pad[n2] = (n2 < N2) ? weights_n2[n2] : '0;
+    for (n1=0; n1<N12_PAD; n1=n1+1) begin
+      assign weights_n1_mag_pad[n1][n2] = (n1<N1/2 && n2<N2) ? weights_n1_mag[n1][n2] : '0;
+      assign weights_n1_pol_pad[n1][n2] = (n1<N1/2 && n2<N2) ? weights_n1_pol[n1][n2] : '0;
+    end
+  end
+
+
+  // Adder Trees
+  logic [D1      :0][N12_PAD-1:0][N2_PAD-1:0][W_A_POL_SUM-1:0] a_pol_tree_i, a_pol_tree_o;
+  logic [D1      :0][N12_PAD-1:0][N2_PAD-1:0][W_A_MAG_SUM-1:0] a_mag_tree_i, a_mag_tree_o;
+  logic [N2_PAD-1:0][W_A_SUM-1:0] sum_a;
+  logic [D2      :0][N2_PAD -1:0][W_Y_SUM-1:0] b_tree;
+
 
 // Multiplication: Binary (+1,-1) NN and regular signed NN
 `define MULTIPLY(K, X) (W_K==1) ? $signed(K ? -X : X) : $signed(K)*$signed(X)
 
-  for (n2=0; n2<N2; n2=n2+1) begin: gN2
+  for (n2=0; n2<N2_PAD; n2=n2+1) begin: gN2
 
 // First layer
     // Multipliers
-    for (n1=0; n1<N1/2; n1=n1+1) begin: gN1
+    for (n1=0; n1<N12_PAD; n1=n1+1) begin: gN1
       always_ff @(posedge clk) begin // Register multiplier ouputs
-        a_mag_tree_o[0][n1][n2] <= `MULTIPLY(weights_n1_mag[n1][n2], in_mag[n1]);
-        a_pol_tree_o[0][n1][n2] <= `MULTIPLY(weights_n1_pol[n1][n2], in_pol[n1]);
+        a_mag_tree_o[0][n1][n2] <= `MULTIPLY(weights_n1_mag_pad[n1][n2], in_mag_pad[n1]);
+        a_pol_tree_o[0][n1][n2] <= `MULTIPLY(weights_n1_pol_pad[n1][n2], in_pol_pad[n1]);
       end
     end
     // Adder Trees, seperate for mag & pol to keep adders small
     for (d=0; d<D1; d=d+1) begin: gD
-      for (a=0; a<N1/2; a=a+1) begin: gA
-        if (a<(N1/2)/2**(d+1)) begin: gR
+      for (a=0; a<N12_PAD/2**(d+1); a=a+1) begin: gA
 
           assign a_pol_tree_i[d+1][a][n2] = $signed(a_pol_tree_o[d][2*a][n2]) + $signed(a_pol_tree_o[d][2*a+1][n2]); 
           assign a_mag_tree_i[d+1][a][n2] = $signed(a_mag_tree_o[d][2*a][n2]) + $signed(a_mag_tree_o[d][2*a+1][n2]);
@@ -65,35 +86,30 @@ module mlp_98 #(
               assign a_pol_tree_o[d+1][a][n2] = a_pol_tree_i[d+1][a][n2];
               assign a_mag_tree_o[d+1][a][n2] = a_mag_tree_i[d+1][a][n2];
           end
-        end else
-          assign {a_pol_tree_o[d+1][a][n2], a_mag_tree_o[d+1][a][n2]} = '0;
       end
     end
     // Register mag + pol sums
     always_ff @(posedge clk)
-      sum_a[n2] <= $signed(a_pol_tree_o[D1-1][0][n2]) + $signed(a_mag_tree_o[D1-1][0][n2]);
+      sum_a[n2] <= $signed(a_pol_tree_o[D1][0][n2]) + $signed(a_mag_tree_o[D1][0][n2]);
 
 // Second layer
     // Multipliers
     always_ff @(posedge clk)           // Register multipliers
-      b_tree[0][n2] <= `MULTIPLY(weights_n2[n2], sum_a[n2]);
+      b_tree[0][n2] <= `MULTIPLY(weights_n2_pad[n2], sum_a[n2]);
   end
 
   // Adder Tree
   for (d=0; d<D2; d=d+1) begin: D
-    for (a=0; a<N2; a=a+1) begin: A
-      if (a<N2/2**(d+1)) begin: R
+    for (a=0; a<N2_PAD/2**(d+1); a=a+1) begin: A
         if (1) // register each stage
           always_ff @(posedge clk)
             b_tree[d+1][a] <= b_tree[d][2*a] + b_tree[d][2*a+1];
         else  // No stages are combinational
             assign b_tree[d+1][a] = b_tree[d][2*a] + b_tree[d][2*a+1];
-      end else 
-        assign b_tree[d+1][a] = '0;
     end
   end
   
-  assign out = b_tree[D2-1][0];
+  assign out = b_tree[D2][0];
 
 endmodule
 
